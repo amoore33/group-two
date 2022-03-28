6551cf84fc1eea3116da302c0ade7b01c51e6b81
amoore33 <71346578+amoore33@users.noreply.github.com>
2022-03-28 13:30:37 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+document.querySelector("#nav").innerHTML = '\
+    <a href="index.html"><div class="logo">\
+       <span class="redbox">G</span>\
+       <span class="redbox">F</span>\
+       <span class="redbox">Y</span>\
+       NEWS\
+    </div></a>\
+    <div class="navlist">\
+        <a href="index.html">HOME</a>\
+        <a href="">WHAT’S HOT</a>\
+        <a href="">POLITICS</a>\
+        <a href="about.html">ABOUT US</a>\
+    </div>\
+';
